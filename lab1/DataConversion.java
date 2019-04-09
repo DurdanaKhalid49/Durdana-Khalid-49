48b2b347b3f75a2f10f7e7d6f8c57aada619f81a
hinasuleiman <49408715+hinasuleiman@users.noreply.github.com>
2019-04-09 17:40:51 +0500

    Add files via upload

---
+++ lab1/DataConversion.java
@@ -0,0 +1,11 @@
+class DataConversion
+{
+	public static void main(String args[])
+	{
+	double x=50.75;
+	long y=(long)50.75;
+	System.out.println("Double:"+x);
+	System.out.println("Conversion of Double:"+y);
+	System.out.println("Fractional Part:"+(x-y));
+	}
+}
---
+++ lab1/Task1.java
@@ -0,0 +1,26 @@
+//Task1
+class Task1
+{
+	public static void main(String [] args)
+	{
+	String n="Durdana";
+	int i=78;
+	short s=88;
+	long l=89;
+	byte b=67;
+	float f=67.99f;
+	double TotalMarks=415.8889d;
+	float percentage=83.887f;
+	char p='%';
+	char grade='A';	 
+System.out.println("My name is ="+n);
+System.out.println("value of i  ="+i);
+System.out.println("value of s ="+s);
+System.out.println("value of b ="+l);
+System.out.println("value of l ="+b);
+System.out.println("value of f is ="+f);
+System.out.println("My Total marks is ="+TotalMarks);
+System.out.println("My percentage is ="+percentage+p);
+System.out.println("My grade is ="+grade);
+	}
+}
